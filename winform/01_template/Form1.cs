82a42cf4909e6fab05dade520537e7e78422d2de
moomint8 <office05053@gmail.com>
2025-02-28 09:24:01 +0900

    first winform

---
+++ winform/01_template/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace _01_template
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
+    }
+}
